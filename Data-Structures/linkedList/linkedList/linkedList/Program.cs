21e4c57366b5b4c8670b1a30e4076c7f903f03c9
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-15 21:35:39 -0700

    edited program

--- Data-Structures/linkedList/linkedList/linkedList/Program.cs
+++ Data-Structures/linkedList/linkedList/linkedList/Program.cs
@@ -10,31 +10,23 @@ namespace linkedList
         {
             Console.WriteLine("Hello World!");
 
-            //Node node1 = new Node(7);
-            //Node node2 = new Node(4);
-            //Node node3 = new Node(13);
-            //Node node4 = new Node(100);
-            //Node node5 = new Node(300);
-            //Node node6 = new Node(25);
+            Node node1 = new Node(7);
+            Node node2 = new Node(4);
+            Node node3 = new Node(13);
+            Node node4 = new Node(100);
+            Node node5 = new Node(300);
+            Node node6 = new Node(25);
 
-            //LinkedList myAwesomeList = new LinkedList(node1);
+            LinkedList myAwesomeList = new LinkedList(node1);
 
-            //myAwesomeList.Add(node2);
-            //myAwesomeList.Add(node3);
-            //myAwesomeList.Add(node4);
+            myAwesomeList.Add(node2);
+            myAwesomeList.Add(node3);
+            myAwesomeList.Add(node4);
 
+            myAwesomeList.AddAfter(node6, node3);
 
-            //myAwesomeList.AddAfter(node6, node3);
+            myAwesomeList.Print();
 
-            //myAwesomeList.Print();
-
-            Node node1 = new Node("bird");
-            Node node2 = new Node(false);
-            Node node3 = new Node(99999);
-            LinkedList testList = new LinkedList(node1);
-            testList.Add(node2);
-            testList.AddAfter(node3, node2);
-            testList.Print();
 
         }
     }
