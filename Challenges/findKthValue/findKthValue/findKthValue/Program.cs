b7b8bfddd4db4ff5a6573db65b52335f1784f203
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-17 09:32:00 -0700

    created test linked list

--- Challenges/findKthValue/findKthValue/findKthValue/Program.cs
+++ Challenges/findKthValue/findKthValue/findKthValue/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using linkedList.classes;
+using System;
 
 namespace findKthValue
 {
@@ -7,6 +8,24 @@ namespace findKthValue
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Node node1 = new Node("banana");
+            Node node2 = new Node(79);
+            Node node3 = new Node(true);
+            Node node4 = new Node("friyah");
+            Node node5 = new Node(300);
+
+            LinkedList testList = new LinkedList(node1);
+            testList.Add(node2);
+            testList.Add(node3);
+            testList.Add(node4);
+            testList.Add(node5);
+
+            testList.Print();
+
+
+
+
         }
     }
 }
