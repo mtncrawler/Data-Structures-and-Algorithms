0257a8b69b441597204a6747bdb884a34bb027da
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-16 23:16:15 -0700

    initial project and test project setup

---
+++ Challenges/findKthValue/findKthValue/findKthValue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace findKthValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
