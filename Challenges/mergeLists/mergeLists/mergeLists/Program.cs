38cb3dbeba13a109bd70134089162959800693a8
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-18 11:37:57 -0700

    passed all tests

--- Challenges/mergeLists/mergeLists/mergeLists/Program.cs
+++ Challenges/mergeLists/mergeLists/mergeLists/Program.cs
@@ -17,18 +17,21 @@ namespace mergeLists
             Node node5 = new Node(5);
             Node node6 = new Node(6);
 
-            LinkedList one = new LinkedList(node3);
-            one.Add(node2);
-            one.Add(node1);
+            LinkedList two = new LinkedList(node3);
+            two.Add(node2);
+            two.Add(node1);
 
-            LinkedList two = new LinkedList(node4);
-            
-            MergeLists(one, two).Print();
+            LinkedList one = new LinkedList(node4);
 
-           
-
+            Console.WriteLine(Print(MergeLists(one, two)));
         }
 
+        /// <summary>
+        /// zip merge two linked lists
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="two"></param>
+        /// <returns>zip merged linked list</returns>
         public static LinkedList MergeLists(LinkedList one, LinkedList two)
         {
             one.Current = one.Head;
@@ -64,14 +67,25 @@ namespace mergeLists
             return one;
         }
 
-        public string Print(LinkedList list)
+        /// <summary>
+        /// prints linked list
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns>string of linked list</returns>
+        public static string Print(LinkedList list)
         {
             list.Current = list.Head;
+            string output = "";
 
-            while (Current.Next !=null)
+            while (list.Current.Next != null)
             {
-
+                output += $"{list.Current.Value} --> ";
+                list.Current = list.Current.Next;
             }
+
+            output += $"{list.Current.Value} --> null";
+            list.Current = list.Head;
+            return output;
         }
     }
 }
