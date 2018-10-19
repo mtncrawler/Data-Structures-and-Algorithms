0578f2250dd20f72bf58a7c77d9344b8dfb3aeee
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-19 16:23:38 -0700

    passing stack tests

--- Data-Structures/Stack and Queue/stackAndQueue/stackAndQueueTest/UnitTest1.cs
+++ Data-Structures/Stack and Queue/stackAndQueue/stackAndQueueTest/UnitTest1.cs
@@ -7,7 +7,43 @@ namespace stackAndQueueTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void PushNodeToStack()
+        {
+            Node node1 = new Node("banana");
+            Node node2 = new Node(20);
+            
+            Stack myStack = new Stack(node1);
+            myStack.Push(node2);
+
+            Assert.Equal(node2, myStack.Peek());
+        }
+
+        [Fact]
+        public void PopNodeFromStack()
+        {
+            Node node1 = new Node("banana");
+            Node node2 = new Node(20);
+
+            Stack myStack = new Stack(node1);
+            myStack.Push(node2);
+
+            Assert.Equal(node2, myStack.Pop());
+        }
+
+        [Fact]
+        public void PeekANodeFromStack()
+        {
+            Node node1 = new Node("banana");
+            Node node2 = new Node(20);
+
+            Stack myStack = new Stack(node1);
+            myStack.Push(node2);
+
+            Assert.Equal(node2, myStack.Peek());
+        }
+
+        [Fact]
+        public void AddNodeToQueue()
         {
 
         }
