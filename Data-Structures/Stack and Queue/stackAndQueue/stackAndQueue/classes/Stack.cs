70f696fe9951ade21969715a683b36ca3cb98888
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-19 15:31:48 -0700

    initial setup; made node and stack classes

---
+++ Data-Structures/Stack and Queue/stackAndQueue/stackAndQueue/classes/Stack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stackAndQueue.classes
+{
+    class Stack
+    {
+        public Node Top { get; set; }
+
+        public Stack(Node node)
+        {
+            Top = node;
+        }
+
+        public Node Peek()
+        {
+            return Top;
+        }
+
+        public Node Pop()
+        {
+            Node temp = Top.Next;
+            Top = Top.Next;
+        }
+    }
+}
