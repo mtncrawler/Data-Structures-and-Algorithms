1888706258b9bbc7b5c52e754df9153d0ad7aa77
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-19 16:30:44 -0700

    passing all tests and added comments

--- Data-Structures/Stack and Queue/stackAndQueue/stackAndQueueTest/UnitTest1.cs
+++ Data-Structures/Stack and Queue/stackAndQueue/stackAndQueueTest/UnitTest1.cs
@@ -6,6 +6,9 @@ namespace stackAndQueueTest
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// test a node is added to stack
+        /// </summary>
         [Fact]
         public void PushNodeToStack()
         {
@@ -18,6 +21,9 @@ namespace stackAndQueueTest
             Assert.Equal(node2, myStack.Peek());
         }
 
+        /// <summary>
+        /// test to remove from stack
+        /// </summary>
         [Fact]
         public void PopNodeFromStack()
         {
@@ -30,6 +36,9 @@ namespace stackAndQueueTest
             Assert.Equal(node2, myStack.Pop());
         }
 
+        /// <summary>
+        /// test to view the top node of stack
+        /// </summary>
         [Fact]
         public void PeekANodeFromStack()
         {
@@ -42,10 +51,46 @@ namespace stackAndQueueTest
             Assert.Equal(node2, myStack.Peek());
         }
 
+        /// <summary>
+        /// test a node is added to a queue
+        /// </summary>
         [Fact]
         public void AddNodeToQueue()
         {
+            Node node1 = new Node("banana");
+            Queue myQueue = new Queue(node1);
 
+            Assert.Equal(node1, myQueue.Peek());
+        }
+
+        /// <summary>
+        /// teste a node is removed from a queue
+        /// </summary>
+        [Fact]
+        public void RemoveNodeFromQueue()
+        {
+            Node node1 = new Node("banana");
+            Node node2 = new Node(20);
+
+            Queue myQueue = new Queue(node1);
+            myQueue.Enqueue(node2);
+
+            Assert.Equal(node1, myQueue.Dequeue());
+        }
+
+        /// <summary>
+        /// test to view the front node from a queue
+        /// </summary>
+        [Fact]
+        public void PeekANodeFromQueue()
+        {
+            Node node1 = new Node("banana");
+            Node node2 = new Node(20);
+
+            Queue myQueue = new Queue(node1);
+            myQueue.Enqueue(node2);
+
+            Assert.Equal(node1, myQueue.Peek());
         }
     }
 }
