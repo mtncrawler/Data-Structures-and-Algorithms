f86703e4f6a28e3398dd8c99bfbb4f7c29b8c33c
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-21 21:25:18 -0700

    initial setup

---
+++ Challenges/linkedListPalindrome/README.md
@@ -0,0 +1,2 @@
+### Validate Linked List Palindrome 
+
---
+++ Challenges/linkedListPalindrome/linkedListPalindrome/linkedListPalindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using linkedList.classes;
+
+
+namespace linkedListPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            ValidatePalindrome(input);
+
+        }
+
+        public static bool ValidatePalindrome(LinkedList input)
+        {
+
+        }
+    }
+}
