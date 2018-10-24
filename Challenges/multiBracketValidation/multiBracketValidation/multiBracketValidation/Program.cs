732e3e97f9302f13edcb80cdd1c785180cd293bf
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-24 13:56:33 -0700

    initial setup

---
+++ Challenges/multiBracketValidation/multiBracketValidation/multiBracketValidation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace multiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MultiBracketValidation();
+        }
+
+        public bool MultiBracketValidation(string input)
+        {
+
+        }
+    }
+}
