359acffff4a6905aa43df3c6730ac1480f4f2c81
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-24 19:24:54 -0700

    passing some tests

--- Challenges/multiBracketValidation/multiBracketValidation/multiBracketValidation/Program.cs
+++ Challenges/multiBracketValidation/multiBracketValidation/multiBracketValidation/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace multiBracketValidation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- Challenges/multiBracketValidation/multiBracketValidation/multiBracketValidationTest/UnitTest1.cs
+++ Challenges/multiBracketValidation/multiBracketValidation/multiBracketValidationTest/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using multiBracketValidation;
 
 namespace multiBracketValidationTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("{{{hello[[()]]}}}()[]")]
+       
+        //[InlineData()]
+        public void TrueBalancedBrackets(string input)
         {
+            Assert.True(Program.MultiBracketValidation(input));
+        }
 
+        [Theory]
+        [InlineData("[({((hi))}]")]
+        [InlineData("doowap")]
+        public void FalseBalancedBrackets(string input)
+        {
+            Assert.False(Program.MultiBracketValidation(input));
         }
     }
 }
