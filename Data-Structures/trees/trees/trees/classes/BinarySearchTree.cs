27847fecb928fe8d52785b1b3a5966d231f7398f
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-10-29 11:02:34 -0700

    completed binarySearchTree add method

--- Data-Structures/trees/trees/trees/classes/BinarySearchTree.cs
+++ Data-Structures/trees/trees/trees/classes/BinarySearchTree.cs
@@ -6,5 +6,36 @@ namespace trees.classes
 {
     class BinarySearchTree
     {
+        public Node Root { get; set; }
+
+        public BinarySearchTree (Node value)
+        {
+            Root = value;
+        }
+
+        public void Add(Node node, Node add)
+        {
+            Node current = node;
+            Node previous = current;
+
+            while (current != null)
+            {
+                if (add.Value < current.Value)
+                {
+                    previous = current;
+                    current = current.LeftChild;
+                }
+                else if (add.Value > current.Value)
+                {
+                    previous = current;
+                    current = current.RightChild;
+                }
+            }
+
+            if (add.Value < previous.Value)
+                previous.LeftChild = add;
+            else
+                previous.RightChild = add;
+        }
     }
 }
