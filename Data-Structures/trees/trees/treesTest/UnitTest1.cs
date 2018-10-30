e08928410163d0d2b39c5e9806283c8a3c532b24
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-10-29 21:03:50 -0700

    passing InOrder test

--- Data-Structures/trees/trees/treesTest/UnitTest1.cs
+++ Data-Structures/trees/trees/treesTest/UnitTest1.cs
@@ -24,7 +24,25 @@ namespace treesTest
             List<int> expected = new List<int> { 25, 700, test };
 
             Assert.Equal(expected, bt.PreOrder(n1, new List<int>()));
+        }
 
+        [Theory]
+        [InlineData(100)]
+        [InlineData(-541)]
+        [InlineData(0.65)]
+        public void InOrderBinaryTree(int test)
+        {
+            Node n1 = new Node(25);
+            Node n2 = new Node(test);
+            Node n3 = new Node(700);
+
+            BinaryTree bt = new BinaryTree(n1);
+            n1.LeftChild = n2;
+            n1.RightChild = n3;
+
+            List<int> expected = new List<int> { test, 25, 700 };
+
+            Assert.Equal(expected, bt.InOrder(n1, new List<int>()));
         }
     }
 }
