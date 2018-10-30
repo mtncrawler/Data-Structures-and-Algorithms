26d05f7cf7dad893589d7f891503cc1ebc97695c
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-10-29 20:41:09 -0700

    completed fizzbuzztree method; no testing req

--- Challenges/fizzBuzzTree/fizzBuzzTree/fizzBuzzTree/Program.cs
+++ Challenges/fizzBuzzTree/fizzBuzzTree/fizzBuzzTree/Program.cs
@@ -1,5 +1,6 @@
-﻿using stackAndQueue.classes;
+﻿using trees.classes;
 using System;
+using System.Collections.Generic;
 
 namespace fizzBuzzTree
 {
@@ -7,14 +8,51 @@ namespace fizzBuzzTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to FizzBuzz Binary Tree!");
 
+            Node n1 = new Node(30);
+            Node n2 = new Node(11);
+            Node n3 = new Node(9);
+            Node n4 = new Node(4);
+            Node n5 = new Node(10);
 
+            n1.LeftChild = n2;
+            n1.RightChild = n3;
+            n1.LeftChild.LeftChild = n4;
+            n1.LeftChild.LeftChild.LeftChild = n5;
+
+            FizzBuzzTree(n1);
         }
 
-        public void FizzBuzzTree(Node node)
+        /// <summary>
+        /// traverses binary tree to writeLine fizz, buzz, or fizzbuzz for node values divisible by 3, 5, or 15 respectively 
+        /// </summary>
+        /// <param name="node">root node of BT</param>
+        public static void FizzBuzzTree(Node node)
         {
+            if (node.LeftChild != null)
+            {
+                FizzBuzzTree(node.LeftChild);
+            }
+            
+            if (node.Value % 15 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (node.Value % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (node.Value % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
 
+            if (node.RightChild != null)
+            {
+                FizzBuzzTree(node.RightChild);
+            }
         }
+        
     }
 }
