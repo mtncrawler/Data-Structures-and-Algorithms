1b37866643b0dfb7d18fb12af2049d68639e0b9f
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-04 15:57:00 -0800

    initial setup

---
+++ Challenges/findMaxInBinaryTree/findMaxInBinaryTree/findMaxInBinaryTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace findMaxInBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
