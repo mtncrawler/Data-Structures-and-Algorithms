d207e043eff3582458e7b63fb8ce15df3ee85442
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-04 18:13:19 -0800

    passing all tests!

--- Challenges/findMaxInBinaryTree/findMaxInBinaryTree/findMaxInBinaryTreeTest/UnitTest1.cs
+++ Challenges/findMaxInBinaryTree/findMaxInBinaryTree/findMaxInBinaryTreeTest/UnitTest1.cs
@@ -8,6 +8,25 @@ namespace findMaxInBinaryTreeTest
     public class UnitTest1
     {
         [Fact]
+        public void TestsForHighestValue()
+        {
+            Node n1 = new Node(10000);
+            Node n2 = new Node(-576);
+            Node n3 = new Node(98);
+            Node n4 = new Node(69);
+            Node n5 = new Node(-335);
+
+            n4.LeftChild = n2;
+            n4.RightChild = n1;
+            n4.RightChild.RightChild = n5;
+            n4.LeftChild.LeftChild = n3;
+
+            BinaryTree bt = new BinaryTree(n4);
+
+            Assert.Equal(n1.Value, Program.FindMaxValue(bt));
+        }
+
+        [Fact]
         public void TestForOneNodeTree()
         {
             Node n1 = new Node(69);
@@ -32,11 +51,5 @@ namespace findMaxInBinaryTreeTest
             Assert.Equal(n3.Value, Program.FindMaxValue(bt));
         }
 
-        [Fact]
-        public void EmptyTree()
-        {
-           Node n1 = new Node();
-           BinaryTree bt = new BinaryTree(n1);
-        }
     }
 }
