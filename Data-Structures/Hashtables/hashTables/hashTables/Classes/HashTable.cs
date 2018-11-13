e67dd757db0b205eca935161e24e9954e54ba1a6
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-12 18:37:11 -0800

    gethash method

--- Data-Structures/Hashtables/hashTables/hashTables/Classes/HashTable.cs
+++ Data-Structures/Hashtables/hashTables/hashTables/Classes/HashTable.cs
@@ -6,6 +6,20 @@ namespace hashTables.Classes
 {
     public class HashTable
     {
+        public Node[] Table { get; set; }
 
+        public 
+
+        public int GetHash(string key)
+        {
+            int hashValue = 0;
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                hashValue += key[i];
+            }
+            
+            return hashValue % key.Length;
+        }
     }
 }
--- Data-Structures/Hashtables/hashTables/hashTables/Classes/Node.cs
+++ Data-Structures/Hashtables/hashTables/hashTables/Classes/Node.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace hashTables.Classes
 {
-    class Node
+    public class Node
     {
         public string Key { get; set; }
 
