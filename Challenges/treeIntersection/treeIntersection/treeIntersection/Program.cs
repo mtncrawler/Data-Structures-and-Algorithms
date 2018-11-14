14b7efc58e46f716dab36330941c77aeaad80e6b
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-13 20:04:53 -0800

    instantiated 2 binary trees; Tree Intersection method traverses bt1 to ht

--- Challenges/treeIntersection/treeIntersection/treeIntersection/Program.cs
+++ Challenges/treeIntersection/treeIntersection/treeIntersection/Program.cs
@@ -11,12 +11,47 @@ namespace treeIntersection
         {
             Console.WriteLine("Hello World!");
 
+            trees.classes.Node n1 = new trees.classes.Node(99);
+            trees.classes.Node n2 = new trees.classes.Node(12);
+            trees.classes.Node n3 = new trees.classes.Node(-100);
+            trees.classes.Node n4 = new trees.classes.Node(36);
+
+            trees.classes.Node n5 = new trees.classes.Node(101);
+            trees.classes.Node n6 = new trees.classes.Node(12);
+            trees.classes.Node n7 = new trees.classes.Node(99);
+            trees.classes.Node n8 = new trees.classes.Node(36);
+
+            n1.LeftChild = n2;
+            n1.RightChild = n3;
+            n2.LeftChild = n4;
+
+            n5.LeftChild = n6;
+            n6.RightChild = n7;
+            n6.LeftChild = n8;
+
+            BinaryTree bt1 = new BinaryTree(n1);
+            BinaryTree bt2 = new BinaryTree(n5);
+
+            TreeIntersection(bt1, bt2);
 
         }
 
-        public List<int> TreeIntersection(BinaryTree bt1, BinaryTree bt2)
+        public static List<int> TreeIntersection(BinaryTree bt1, BinaryTree bt2)
         {
             HashTable ht = new HashTable();
+            ht.Table = new hashTables.Classes.Node[1024];
+            ht.Size = 1024;
+
+            List<int> binaryTree1Output = null;
+            List<int> binaryTree2Output = null;
+
+            foreach (var item in bt1.InOrder(bt1.Root, binaryTree1Output))
+            {
+                ht.Add(item.ToString(), item.ToString());
+            }
+
+
+            return;
         }
     }
 }
