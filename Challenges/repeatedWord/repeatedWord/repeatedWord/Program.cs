ffda2ba3e293b0d4b279f070de8719f3c68718ec
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-14 15:06:02 -0800

    initial setup; test proj and readme

---
+++ Challenges/repeatedWord/repeatedWord/repeatedWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace repeatedWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
