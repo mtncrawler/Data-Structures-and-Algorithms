65ea60f742f737bff5f8bff70d08468901944cd6
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-19 14:46:07 -0800

    initial setup

---
+++ Data-Structures/Sorting-Algorithms/insertionSort/insertionSort/insertionSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace insertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
