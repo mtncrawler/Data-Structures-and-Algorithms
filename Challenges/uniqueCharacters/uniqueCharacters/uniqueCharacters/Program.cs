bc22506b2bdda4fd90356c6265fcf7055bff8758
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-18 18:34:51 -0800

    initial setup

---
+++ Challenges/uniqueCharacters/uniqueCharacters/uniqueCharacters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uniqueCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        public static bool 
+
+        
+    }
+}
---
+++ Challenges/uniqueCharacters/uniqueCharacters/uniqueCharactersTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace uniqueCharactersTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
