f5e94112aa33fd3493e4ab210fc77c0ef06944ab
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-21 14:53:10 -0800

    initial setup

---
+++ Data-Structures/Sorting-Algorithms/mergeSort/mergeSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace mergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        static void MergeSort(int[] myarr)
+        {
+            if (myarr.Length > 1 )
+            {
+                int leftSize = myarr.Length / 2;
+                int rightSize = myarr.Length / 2;
+
+                int[] left = new int[leftSize];
+                Array.Copy(myarr, 0, left, 0, leftSize);
+
+                int[] right = new int[rightSize];
+                Array.Copy(myarr, myarr.Length / 2, right, 0, rightSize);
+
+                MergeSort(left);
+                MergeSort(right);
+
+            }
+        }
+}
