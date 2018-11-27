d257a596e18c6c012a0233bac89615ef0b4e2e23
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-27 11:18:27 -0800

    created graph and node class

---
+++ Data-Structures/graph/graph/graph/Graph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace graph
+{
+    class Graph
+    {
+        public LinkedList<LinkedList<Tuple<Node, int>>> AdjacencyList { get; set; }
+
+        public Graph()
+        {
+            AdjacencyList = new LinkedList<LinkedList<Tuple<Node, int>>>();
+        }
+
+
+    }
+}
---
+++ Data-Structures/graph/graph/graph/Node.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace graph
+{
+    class Node
+    {
+        public string Value { get; set; }
+    }
+}
